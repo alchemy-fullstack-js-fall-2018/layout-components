872699859dc96a49b08799cce60026873e75e63b
Alex Rankin <alex.c.rankin@gmail.com>
2018-11-12 17:41:46 -0800

    rendering my sandwich header!

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,16 @@ import Footer from './Footer';
 
 export default class App extends Component {
   render() {
+    const header = new Header({
+      links: ['banh mi', 'deli', 'subs']
+    });
+    const footer = new Footer();
 
     return `
-      <h1>${this.props.message}</h1>
+    ${header.render()}
+    <h1>${this.props.message}</h1>
     `;
   }
 }
+
+// ${footer.render()}
