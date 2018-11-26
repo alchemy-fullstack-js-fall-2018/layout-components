cd078b68c6b87f12e09a02cd51012718319cfe45
Ryan Mehta <ryan.mehta@gmail.com>
2018-11-25 21:16:57 -0800

    Revert "Tues PM PR"

--- src/components/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import Footer from './Footer';
-describe('footer', () => {
-  it('should be a footer', () => {
-    const footer = new Footer({
-      text: '&copy; 2018 Alex Rankin'
-    });
-    expect(footer.render()).toMatchSnapshot();
-  });
-});
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import App from './components/App';
-
-const app = new App({
-  message: 'hey hey hey'
-});
-
-app.attach(document.getElementById('root'));
